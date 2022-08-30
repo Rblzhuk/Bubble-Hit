5670232d6f18e5fc9b922130df255a091b7e0a01
Rblzhuk <shilnikovdmitrievich@yandex.ru>
2022-08-30 15:17:08 +0300

    1commit

---
+++ SDCourseProject/Ball.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDCourseProject
+{
+    class Ball
+    {
+        public int x;
+        public int y;
+        public Ball(int ballX,int ballY)
+        {
+            x = ballX;
+            y = ballY;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Ball)
+            {
+                Ball c = (Ball)obj;
+                return x == c.x && y == c.y;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return x.GetHashCode() ^ y.GetHashCode();
+        }
+    }
+}
